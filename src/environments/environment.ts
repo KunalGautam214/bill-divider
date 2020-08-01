fc45bf5355ddd741ad5b4643e26bc0aa8100e447
Kunal Gautam <62873524+KunalGautam214@users.noreply.github.com>
2020-08-01 14:49:36 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,17 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyA3PyPN52zgqzsKKRifFRM18NNlLOGXq8g",
-    authDomain: "bill-divider-ff6a1.firebaseapp.com",
-    databaseURL: "https://bill-divider-ff6a1.firebaseio.com",
-    projectId: "bill-divider-ff6a1",
-    storageBucket: "bill-divider-ff6a1.appspot.com",
-    messagingSenderId: "772238095281",
-    appId: "1:772238095281:web:4790c4b9e0a7c0c8db4390",
-    measurementId: "G-4CGRBP17DF"
-  }
+  production: false
 };
 
 /*
